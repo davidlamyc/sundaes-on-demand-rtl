4581e8b07238c4e70befe6804fa9e88b65ae10c8
David Lam <davidlam.yc@gmail.com>
2022-05-14 21:07:11 +0800

    mre tests

---
+++ src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from '../../../test-utils/testing-library-utils';
+import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
+
+import Options from '../Options';
+
+test('displays image for each scoop option from server', async () => {
+    render(<Options optionType="scoops" />);
+
+    const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
+    expect(scoopImages).toHaveLength(2);
+
+    const altText = scoopImages.map(element => element.alt);
+    expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('displays image for each topping option from server', async () => {
+    render(<Options optionType="toppings" />);
+
+    const images = await screen.findAllByRole('img', { name: /topping$/i });
+    expect(images).toHaveLength(3);
+
+    const imageTitles = images.map(img => img.alt);
+    expect(imageTitles).toEqual([
+        'Cherries topping',
+        'M&Ms topping',
+        'Hot fudge topping'
+    ])
+})
